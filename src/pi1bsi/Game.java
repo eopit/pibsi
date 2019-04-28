223fe46d6945c723bd6afa0465d0dbb1e60485bb
Pedro Henrique <pedro.barricelli@gmail.com>
2019-04-28 20:20:18 -0300

    First decision made OK

--- src/pi1bsi/Game.java
+++ src/pi1bsi/Game.java
@@ -6,12 +6,22 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) throws InterruptedException, IOException { // main com o Thread para sleep
         Scanner sc = new Scanner(System.in);
+        String decision;
         Printer.clearConsole();
-        Printer.printWatch();
-        System.out.println("Voce acordou com o barulho do despertador, deseja fazer o que?");
-        System.out.println("Levantar ou mexer no celular?");
-        sc.nextLine();
+//        Printer.printWatch();
+        System.out.println("Voce acorda com o barulho do despertador para ir trabalhar, o que deseja fazer?");
+        System.out.println("A. Levantar\nB. Mexer no celular mais um pouco");
+
+        do {
+            decision = sc.nextLine();
+
+            if (!decision.equalsIgnoreCase("Levantar") && !decision.equalsIgnoreCase("Mexer no celular mais um pouco")) {
+                System.err.println("Digite apenas exatamente o que aparece nas alternativas!");
+            }
+
+        } while (!decision.equalsIgnoreCase("Levantar") && !decision.equalsIgnoreCase("Mexer no celular mais um pouco"));
     }
 }
+
 
 
