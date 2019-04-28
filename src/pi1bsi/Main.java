78e4044a7032fb1207fcde06fce449e6d9c12029
Pedro Henrique <pedro.barricelli@gmail.com>
2019-04-28 15:05:08 -0300

    testando adicionar novos arquivos

--- src/pi1bsi/Main.java
+++ src/pi1bsi/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("teste");
+        System.err.println("fita");
     }
 }
---
+++ src/pi1bsi/Teste1.java
@@ -0,0 +1,7 @@
+package pi1bsi;
+
+public class Teste1 {
+    void teste() {
+        System.out.println("a");
+    }
+}
