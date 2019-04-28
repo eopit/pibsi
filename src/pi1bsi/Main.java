65e620946d4daf7fa5899b0ed61372bcbcb437ca
Pedro Henrique <pedro.barricelli@gmail.com>
2019-04-28 16:37:02 -0300

    relogio

--- src/pi1bsi/Main.java
+++ src/pi1bsi/Main.java
@@ -3,6 +3,15 @@ package pi1bsi;
 public class Main {
 
     public static void main(String[] args) {
-        //main code
+
+        System.out.println("       .-.-.");
+        System.out.println("  ((  (__I__)  ))"); //DEIXAR OS PARENTESES SOMENTE QUANDO FOR TOCAR
+        System.out.println("    .'_....._'.");
+        System.out.println("   / / .12. \\ \\");
+        System.out.println("  | | '     ' | |");
+        System.out.println("  | | 9     3 | |");
+        System.out.println("   \\ \\ '.6.' / /");
+        System.out.println("    '.`-...-'.'");
+        System.out.println("     /'-- --'\\");
     }
 }
